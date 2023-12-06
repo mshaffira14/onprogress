161e46d6b50f7085d3f10d4ce12e5c5056bc4d2d
magang UDI 5 <magang5@psi.dinus.ac.id>
2023-12-06 14:56:06 +0700

    p tato

--- ardi/python/qt5/main.py
+++ ardi/python/qt5/main.py
@@ -1,40 +1,124 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QFileDialog
-from PyQt5.QtGui import QPixmap
-from PIL import Image, ImageTk
-import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtGui import QPixmap, QImage
+from desain import Ui_Form
+from PIL import Image
+
+
 class ImageEncryptionApp(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        # Tambahkan kode inisialisasi atau hubungkan sinyal-sinyal di sini
+        # Buat atribut untuk menyimpan gambar
+        self.original_image = None
+        self.encrypted_image = None
+
+        # Connect the signals to slots
+        self.pushButton_4.clicked.connect(self.upload_image)
+        self.pushButton_2.clicked.connect(self.encrypt_image)
+        self.pushButton_3.clicked.connect(self.decrypt_image)
+        self.pushButton.clicked.connect(self.save_file)
 
     def upload_image(self):
-        global original_image
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select Image File", "", "Image Files (*.png *.jpg *.bmp)")
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Select Image File", "", "Image Files (*.png *.jpg *.bmp)")
         if file_path:
-            original_image = Image.open(file_path)
-            original_image = original_image.resize((300, 200), Image.LANCZOS)
-            photo = ImageTk.PhotoImage(original_image)
-            self.label_image_before.setPixmap(QPixmap(photo))
+            self.original_image = Image.open(file_path)
+            self.original_image = self.original_image.resize(
+                (201, 191), Image.LANCZOS)
+
+            # Convert PIL Image to QImage
+            q_image = QImage(self.original_image.tobytes(), self.original_image.width, self.original_image.height,
+                             self.original_image.width * 3, QImage.Format_RGB888)
+
+            # Convert QImage to QPixmap
+            pixmap = QPixmap.fromImage(q_image)
+            self.frame_3.setPixmap(pixmap)
 
     def encrypt_image(self):
-        global original_image, encrypted_image
         key = self.lineEdit.text().upper()
-        key_index = 0
-        encrypted_pixels = []
 
-        # ... (kode enkripsi dari main.py)
+        # Convert key to bytes
+        key_bytes = key.encode('utf-8')
 
-        encrypted_image = Image.new(original_image.mode, original_image.size)
-        encrypted_image.putdata(encrypted_pixels)
-        encrypted_image = encrypted_image.resize((300, 200), Image.LANCZOS)
+        # Step 1: Beaufort Cipher
+        beaufort_cipher = bytearray()
+        for i in range(len(self.original_image.tobytes())):
+            char_original = self.original_image.tobytes()[i]
+            char_key = key_bytes[i % len(key_bytes)]
+            char_cipher = (char_key - char_original) % 256
+            beaufort_cipher.append(char_cipher)
 
-        photo = ImageTk.PhotoImage(encrypted_image)
-        self.label_image_after.setPixmap(QPixmap(photo))
+        # Step 2: Vigenere Cipher
+        vigenere_cipher = bytearray()
+        for i in range(len(beaufort_cipher)):
+            char_original = beaufort_cipher[i]
+            char_key = key_bytes[i % len(key_bytes)]
+            char_cipher = (char_original + char_key) % 256
+            vigenere_cipher.append(char_cipher)
+
+        # Create a new PIL Image from the encrypted bytes
+        encrypted_image = Image.frombytes(
+            "RGB", self.original_image.size, bytes(vigenere_cipher))
+
+        # Convert PIL Image to QImage
+        q_image = QImage(encrypted_image.tobytes(), encrypted_image.width, encrypted_image.height,
+                         encrypted_image.width * 3, QImage.Format_RGB888)
+
+        # Convert QImage to QPixmap
+        pixmap = QPixmap.fromImage(q_image)
+        self.frame_4.setPixmap(pixmap)
+
+        # Set the encrypted image attribute
+        self.encrypted_image = encrypted_image
+
+    def decrypt_image(self):
+        key = self.lineEdit.text().upper()
+
+        # Convert key to bytes
+        key_bytes = key.encode('utf-8')
+
+        # Step 1: Beaufort Cipher
+        beaufort_cipher = bytearray()
+        for i in range(len(self.original_image.tobytes())):
+            char_original = self.original_image.tobytes()[i]
+            char_key = key_bytes[i % len(key_bytes)]
+            char_cipher = (char_key + char_original) % 256
+            beaufort_cipher.append(char_cipher)
+
+        # Step 2: Vigenere Cipher
+        vigenere_cipher = bytearray()
+        for i in range(len(beaufort_cipher)):
+            char_original = beaufort_cipher[i]
+            char_key = key_bytes[i % len(key_bytes)]
+            char_cipher = (char_original - char_key) % 256
+            vigenere_cipher.append(char_cipher)
+
+        # Create a new PIL Image from the encrypted bytes
+        encrypted_image = Image.frombytes(
+            "RGB", self.original_image.size, bytes(vigenere_cipher))
+
+        # Convert PIL Image to QImage
+        q_image = QImage(encrypted_image.tobytes(), encrypted_image.width, encrypted_image.height,
+                         encrypted_image.width * 3, QImage.Format_RGB888)
+
+        # Convert QImage to QPixmap
+        pixmap = QPixmap.fromImage(q_image)
+        self.frame_4.setPixmap(pixmap)
+
+        # Set the encrypted image attribute
+        self.encrypted_image = encrypted_image
+
+    def save_file(self):
+        if self.encrypted_image:
+            file_path, _ = QFileDialog.getSaveFileName(
+                self, "Save Image File", "", "Image Files (*.png *.jpg *.bmp)")
+            if file_path:
+                self.encrypted_image.save(file_path)
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     window = ImageEncryptionApp()
     window.show()
-    sys.exit(app.exec_())
+    app.exec_()
